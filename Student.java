960351ee7b256ce770fd7c23cd874c148d88bf7b
YsmaelK <99379297+YsmaelK@users.noreply.github.com>
2023-03-12 19:20:43 -0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,50 +1,19 @@
+//5A
 public class Student {
     private String name;
-    private String major;
-    private double gpa;
-    
-    public Student() {}
-    
-    public Student(String name, String major) {
+    private Transcript transcript;
+
+    public Student(String name) {
         this.name = name;
-        this.major = major;
     }
-    
-    public Student(String name, String major, double gpa) {
-        this.name = name;
-        this.major = major;
-        this.gpa = gpa;
-    }
-    
+
+    // Getters and setters
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
-    public String getMajor() {
-        return major;
-    }
-    
-    public void setMajor(String major) {
-        this.major = major;
-    }
-    
-    public double getGpa() {
-        return gpa;
-    }
-    
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-    
-    public String toString() {
-        return "Name: " + name + ", Major: " + major + ", GPA: " + gpa;
-    }
-    
-    public boolean equals(Student student) {
-        return (this.name.equals(student.name) && this.major.equals(student.major) && this.gpa == student.gpa);
-    }
 }
+
